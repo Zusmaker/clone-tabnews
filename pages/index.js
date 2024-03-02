e8a7bf2226984ae1722b202ecee66e4f1c0ab7ef
Zus <127681742+Zusmaker@users.noreply.github.com>
2024-03-02 21:46:19 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Querido(a) aventureiro(a), receba as saudações da Zusmaker! 🧙🎲🐲 </h1>
+  return (
+    <h1>Querido(a) aventureiro(a), receba as saudações da Zusmaker! 🧙🎲🐲 </h1>
+  );
 }
 
 export default Home;
